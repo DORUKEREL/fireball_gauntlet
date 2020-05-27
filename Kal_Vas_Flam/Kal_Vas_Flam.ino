17cde66e529ad06614f21355694e599572b65beb
DORUKEREL <66027870+DORUKEREL@users.noreply.github.com>
2020-05-27 19:52:57 +0300

    Update Kal_Vas_Flam.ino

--- Kal_Vas_Flam/Kal_Vas_Flam.ino
+++ Kal_Vas_Flam/Kal_Vas_Flam.ino
@@ -14,20 +14,20 @@ struct Vector {
 
 float limit = 16000; 
 Vector acc;
-
+                   
 float trigger = 1.5;
 float control = 1.5;
-
+                   
 int delayTime = 50;
 int ledBlinkTime = 100;
-
+                      
 int led[] = {2,3,4};
 int plug[] = {12,11,10};
 int pCount = sizeof(plug) / sizeof(int);
-
-int cp = 0;
-int fireTime = 1000;
-
+                          
+int cp = 0;          
+int fireTime = 1000; 
+                
 void setup() {
   Wire.begin();
   Wire.beginTransmission(MPU_addr);
@@ -41,11 +41,11 @@ void setup() {
     pinMode(plug[i], OUTPUT);
     digitalWrite(plug[i], LOW); 
   }
-
+                  
   intro();
 }
-
-
+               
+                 
 void loop() {
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x3B);
@@ -54,7 +54,7 @@ void loop() {
   acc.x = Wire.read()<<8|Wire.read();    
   acc.y = Wire.read()<<8|Wire.read();
   acc.z = Wire.read()<<8|Wire.read();
-
+                    
   acc.x /= limit;
   acc.y /= limit;
   acc.z /= limit;
@@ -62,7 +62,7 @@ void loop() {
   fireballCheck();
   delay(delayTime);
 }
-
+                    
 void fireballCheck() {
   if(acc.x > trigger && abs(acc.y) < control && abs(acc.z) < control) {
     if(cp < pCount) {  
@@ -74,7 +74,7 @@ void fireballCheck() {
     }
   }
 }
-
+                 
 void fireball() {
   digitalWrite(plug[cp], HIGH);
   Serial.println("Firing barrel: " + String(cp));
@@ -83,7 +83,7 @@ void fireball() {
   blinkLed(10, LOW, cp); //blink the leds for another second to prevent a misfire after firing
   cp++;
 }
-
+               
 void intro() {
     ledToggle(0, HIGH);
     delay(300);    
@@ -103,11 +103,11 @@ void intro() {
     delay(300);      
     blinkLeds(3, HIGH);
 }
-
+                       
 void ledToggle(int ledId, int ledState) {
   digitalWrite(led[ledId], ledState);
 }
-
+                    
 void blinkLeds(int blinkCount, int endState) {
   //blink all leds
   for(int i = 0; i < blinkCount; i++) {
@@ -121,12 +121,12 @@ void blinkLeds(int blinkCount, int endState) {
     }    
     delay(ledBlinkTime);    
   }  
-
+                    
   for(int i = 0; i < pCount; i++) {
       ledToggle(i, endState);
   }    
 }
-
+                  
 void blinkLed(int blinkCount, int endState, int ledId) {
   //blink single leds
   for(int i = 0; i < blinkCount; i++) {
@@ -135,6 +135,7 @@ void blinkLed(int blinkCount, int endState, int ledId) {
     ledToggle(ledId, LOW);
     delay(ledBlinkTime);    
   }  
-
+                 
   ledToggle(ledId, endState);
 }
+              
